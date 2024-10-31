06c6d077a2bde8c484be6fcbe87379cdf3c91296
Kevin <barbierikevinm@gmail.com>
2024-10-31 13:04:26 -0400

    Started website, added stubs, added navbar

---
+++ website/server.js
@@ -0,0 +1,2 @@
+"use strict";
+//this will be where the models will be hosted, users will submit their text input through the website (get request) and the server will return the models' results.
---
+++ website/static/scripts/main.js
@@ -0,0 +1,55 @@
+"use strict";
+(function(){
+    window.addEventListener("load", init);
+    
+    function init() {
+        qs("form button").addEventListener('click', (e) => {
+            e.preventDefault();
+            sendData();
+            open("results.html", "_self");
+        });
+    }
+
+    function sendData() {
+        console.log("send data");
+    }
+    /* ------------------------------ Helper Functions  ------------------------------ */
+
+    /**
+     * Returns the element that has the ID attribute with the specified value.
+     * @param {string} id - element ID
+     * @returns {object} DOM object associated with id.
+     */
+    function id(idName) {
+        return document.getElementById(idName);
+    }
+
+    /**
+     * Returns the first element that matches the given CSS selector.
+     * @param {string} query - CSS query selector.
+     * @returns {object} The first DOM object matching the query.
+     */
+    function qs(query) {
+        return document.querySelector(query);
+    }
+
+    /**
+     * Returns the array of elements that match the given CSS selector.
+     * @param {string} query - CSS query selector
+     * @returns {object[]} array of DOM objects matching the query.
+     */
+    function qsa(query) {
+        return document.querySelectorAll(query);
+    }
+
+    /**
+     * This function needs documentation.
+     * @returns {} response
+     */
+    function checkStatus(response) {
+        if (!response.ok) {
+            throw Error("Error in request: " + response.statusText);
+        }
+        return response;
+    }
+})()
